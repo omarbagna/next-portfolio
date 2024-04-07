0a282dcb23d6395da6e64df59d996d9cb11aa4d6
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2024-04-07 23:27:47 +0000

    code: built portfolio in nextjs

---
+++ out/_next/static/chunks/app/page-137d806ee0020130.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7189:function(n,e,u){Promise.resolve().then(u.t.bind(u,6807,23))},6807:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=7189)}),_N_E=n.O()}]);
